76437d56b97bee81d4755ad127226f1325b3f19b
Jordan S <goglobalpioneers@gmail.com>
2016-12-13 16:12:07 -0500

    create counter.js

---
+++ counter.js
@@ -0,0 +1,10 @@
+module.exports.counter = function(arr) {
+	return "There are " + arr.length + " elements in this array";
+};
+
+
+module.exports.adder = function(a,b){
+	return `The sum of the 2 numbers is ${a+b}`;
+}
+
+//template string ES6 - use of bactics - can embedd variables/expressions without cancatinating 
